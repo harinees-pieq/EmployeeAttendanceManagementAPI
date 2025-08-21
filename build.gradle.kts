b63c4e89f861e8e6daa126f975ba47c97eb07b71
harinees-pieq <harinee.s@pieq.ai>
2025-08-21 12:57:58 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,15 +17,15 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("io.dropwizard:dropwizard-bom:4.0.15"))
-    implementation("io.dropwizard:dropwizard-core")
-    implementation(kotlin("stdlib"))
+    implementation(platform("io.dropwizard:dropwizard-bom:4.0.15")) //ensure version of all dropwizard
+    implementation("io.dropwizard:dropwizard-core") //core functionality like REST API, config
+    implementation(kotlin("stdlib")) //basic kotlin functionality
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //json serialize and deserialize
+    implementation("org.jdbi:jdbi3-kotlin:3.45.2") //sql orm (object relational mapper)
+    implementation("io.dropwizard:dropwizard-jdbi3:4.0.7") //jdbi v3
+    implementation("org.postgresql:postgresql:42.7.3") //postgres connection
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1") //java 8+ local date
     testImplementation(kotlin("test"))
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jdbi:jdbi3-kotlin:3.45.2")
-    implementation("io.dropwizard:dropwizard-jdbi3:4.0.7")
-    implementation("org.postgresql:postgresql:42.7.3")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     testImplementation("io.mockk:mockk:1.13.10")
