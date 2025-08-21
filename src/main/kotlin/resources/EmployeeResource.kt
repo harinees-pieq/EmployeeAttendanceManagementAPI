bb1bf1c36dc20a2a3c1bbc5260d9aede3ca0af5e
harinees-pieq <harinee.s@pieq.ai>
2025-08-21 10:59:34 +0530

    Update EmployeeResource.kt

--- src/main/kotlin/resources/EmployeeResource.kt
+++ src/main/kotlin/resources/EmployeeResource.kt
@@ -1,9 +1,13 @@
 package resources
 
+//APIs
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.Response
+//model
 import model.EmployeeData
+//service
 import services.EmployeeService
+//exception
 import java.lang.IllegalArgumentException
 
 @Path("/employees")
@@ -23,7 +27,7 @@ class EmployeeResource(private val employeeService: EmployeeService) {
     fun addEmployee(employeeData: EmployeeData): Response {
         return try {
             val createdEmployee = employeeService.create(employeeData)
-            Response.status(Response.Status.CREATED).entity(createdEmployee).build()
+            Response.status(Response.Status.OK).entity(createdEmployee).build()
         } catch (e: IllegalArgumentException) {
             Response.status(Response.Status.BAD_REQUEST).entity(mapOf("error" to e.message)).build()
         }
